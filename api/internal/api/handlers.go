44c60a63c2fa9085c80a64b4227cff34dbb648ed
Thiago Henrique <68298881+ts-dart@users.noreply.github.com>
2025-03-07 14:20:06 -0300

    Update handlers.go

--- api/internal/api/handlers.go
+++ api/internal/api/handlers.go
@@ -1,5 +1,23 @@
 package api
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
 func handlers() {
 	s := postsBySection("")
+
+	http.HandleFunc("/postsBySection", helloHandler)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Definindo o cabeçalho da resposta como JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Criando a resposta
+	response := Response{Message: "Olá, mundo!"}
+
+	// Convertendo para JSON e enviando a resposta
+	json.NewEncoder(w).Encode(response)
 }
