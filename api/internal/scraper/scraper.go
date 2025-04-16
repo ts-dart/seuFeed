3c426a31dca411b97c6c47a7413983394b0a412a
thiago <thiagoedusan5.11@outlook.com>
2025-04-16 12:13:09 -0300

    feat: Adicionado uma condição para que PostsList seja zerado a cada loop. feito tambem uma nova modificação para que os intervalos entre os loops seja feito de forma randomica

--- api/internal/scraper/scraper.go
+++ api/internal/scraper/scraper.go
@@ -1,11 +1,12 @@
 package scraper
 
 import (
-	"fmt"
-	"time"
 	"encoding/json"
-	"os"
+	"fmt"
 	"log"
+	"math/rand"
+	"os"
+	"time"
 )
 
 type Post struct {
@@ -22,14 +23,23 @@ var PostsList []Post = make([]Post, 0, 100)
 
 func Scraper() {
 	for {
+		if len(PostsList) > 0 {
+			PostsList = PostsList[:0]
+		}
+
 		handleScraperErrors(urlo)
 		handleScraperErrors(urlt)
 
 		writeJson(PostsList)
 		fmt.Println(PostsList)
 
-		time.Sleep(3 * time.Minute)
+		time.Sleep(time.Duration(randomNumber()) * time.Minute)
 	}
+}
+
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+func randomNumber() (int) {
+	return rng.Intn(8) + 3
 }
 
 // Função para lidar com erros nos scrapers sem interromper o fluxo
