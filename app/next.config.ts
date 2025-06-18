29a3ab4289c468d7c3b4abab15ddadad941ccc9a
thiago <thiagoedusan5.11@outlook.com>
2025-06-18 02:12:56 -0300

    fix: alterações feitas no next.config.ts para que seja aceita imagens de quaisquer dominios

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,16 +1,21 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        domains: ['www.cnnbrasil.com.br', 's2-g1.glbimg.com', 'encrypted-tbn0.gstatic.com'],
-    },
-    redirects: async () => [
-        {
-            source: "/",
-            destination: "/home",
-            permanent: true,
-        },
+  images: {
+    remotePatterns: [
+      {
+        protocol: undefined, // aceita http e https
+        hostname: '**',      // aceita qualquer domínio
+      },
     ],
+  },
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/home",
+      permanent: true,
+    },
+  ],
 };
 
 export default nextConfig;
